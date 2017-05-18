b6d8a1e8eeda6d4dc6d40c82c3cfa7cc472d8168
Romain Ferron <romain.ferron@hetic.net>
2017-05-18 16:53:03 +0200

    Last last work

---
+++ revue.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ferro
+ * Date: 15/05/2017
+ * Time: 22:25
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>L'architecture</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+    <main>
+
+        <?php include 'header.php';?>
+
+        <body class="wrap">
+        <div class="title">
+            <h1>Champagne-Ardenne-Picardie-Nord-Pas-de-Calais</h1>
+            <h2>N°285-2017</h2>
+        </div>
+
+        <div class="mainarticle">
+        <div class="article  col-5">
+            <img class="magazine" src="content/couv_277.jpg" alt="magazine277"> <br>
+            <a href="#">Commander</a>
+        </div>
+        <div class="partenaire clearfix col-6">
+            <h2>Nos partenaires</h2>
+            <p>Agencement, mobilier, métier d'art, décoration, cheminées</p>
+            <ul>
+                <li>Euresco (69)</li>
+                <li>Leroux - Pacreau (49)</li>
+            </ul>
+            <p>Agencement, mobilier, métier d'art, décoration, cheminées</p>
+            <ul>
+                <li>Euresco (69)</li>
+                <li>Leroux - Pacreau (49)</li>
+            </ul>
+            <p>Agencement, mobilier, métier d'art, décoration, cheminées</p>
+            <ul>
+                <li>Euresco (69)</li>
+                <li>Leroux - Pacreau (49)</li>
+            </ul>
+            <p>Agencement, mobilier, métier d'art, décoration, cheminées</p>
+            <ul>
+                <li>Euresco (69)</li>
+                <li>Leroux - Pacreau (49)</li>
+            </ul>
+        </div>
+        </div>
+        </body>
+    </main>
+</body>
+</html>
